30a478271770e846998434db0b347d13bd130fe4
HOAN <hoan.c9907@gmail.com>
2025-07-01 23:40:06 +0900

    feat: vite.config.ts 일부 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // 모든 외부 IP 허용
+    // host: '0.0.0.0',
+    host: true,
     port: 5173,
-    https: {}, // HTTPS 사용 (빈 객체를 사용하여 기본 설정 적용)
   },
   preview: {
     host: '0.0.0.0',
