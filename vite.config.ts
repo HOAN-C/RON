b5aff8d28e1a6e9d6b0ff09d73d865c8b83fabfc
HOAN <hoan.c9907@gmail.com>
2025-07-02 06:51:54 +0900

    feat/fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0', // 모든 외부 IP 허용
     port: 5173,
   },
   preview: {
